29ade68cd0d5266053dc04c21055a27ff7617485
Saksham <123803359+SakshamDev2005@users.noreply.github.com>
2024-08-24 15:58:44 +0530

    Add files via upload

---
+++ AI-Agent.py
@@ -0,0 +1,30 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_groq import ChatGroq
+
+
+model = ChatGroq(model="llama3-8b-8192",api_key='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')
+
+template = """
+You're an AI Translator.
+You translate texts in different languages as per the user's wish.
+
+Question: {question}
+
+Answer:
+
+"""
+
+prompt = PromptTemplate(template = template, input_variables=['question'])
+
+while True:
+    user_inp = input('Ask the Translator ->')
+
+    if user_inp.lower() == 'stop':
+        break
+    chain = LLMChain(llm = model, prompt = prompt)
+    print(chain.run(user_inp))
